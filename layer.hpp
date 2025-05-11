12d8fc66ab8d950d45f4f7486de079a81c0f59ad
anshm42 <69057488+anshm42@users.noreply.github.com>
2025-05-11 19:37:36 -0400

    feat: added fwd prop in neuralnet class

--- layer.hpp
+++ layer.hpp
@@ -4,27 +4,28 @@ using namespace Eigen;
 
 class Layer {
   private:
-    VectorXd output;
+    VectorXd activations;
 
     MatrixXd weights;
     VectorXd biases;
 
   public:
-    Layer(int input_size, int output_size) {
-        weights = MatrixXd::Random(input_size, output_size);
-        biases = VectorXd::Random(output_size);
+    Layer(int inputSize, int outputSize) {
+        weights = MatrixXd::Random(inputSize, outputSize);
+        biases = VectorXd::Random(outputSize);
     }
 
     VectorXd sigmoid(const VectorXd &v) {
         return 1.0 / (1.0 + (-v.array()).exp());
     }
 
-    VectorXd sigmoid_derivative(const VectorXd &v) {
+    VectorXd sigmoidDerivative(const VectorXd &v) {
         return sigmoid(v).array() * (1.0 - sigmoid(v).array());
     }
 
-    VectorXd forward(const VectorXd &input) {
-        output = sigmoid((input * weights) + biases);
-        return output;
+    void forward(const VectorXd &input) {
+        activations = sigmoid((input * weights) + biases);
     }
+
+    VectorXd getActivations() { return activations; }
 };
--- neuralnet.hpp
+++ neuralnet.hpp
@@ -13,7 +13,12 @@ class NeuralNet {
     void runNeuralNet(const VectorXd &input) {
         layers[0].forward(input);
         for (size_t i = 0; i < layers.size(); i++) {
-            layers[i].forward(layers[i - 1].output);
+            layers[i].forward(layers[i - 1].getActivations());
         }
+    }
+
+    VectorXd getPrediction() {
+        Layer predictionLayer = layers.back();
+        return predictionLayer.getActivations();
     }
 };
