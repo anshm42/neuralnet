38037a4e234a4cd6335c96bbab7d009e16c7b0e4
anshm42 <69057488+anshm42@users.noreply.github.com>
2025-05-11 19:29:05 -0400

    feat: added neuralnet class

---
+++ neuralnet.hpp
@@ -0,0 +1,19 @@
+#include "layer.hpp"
+#include <vector>
+
+using namespace Eigen;
+
+class NeuralNet {
+  private:
+    std::vector<Layer> layers;
+
+  public:
+    NeuralNet() {}
+
+    void runNeuralNet(const VectorXd &input) {
+        layers[0].forward(input);
+        for (size_t i = 0; i < layers.size(); i++) {
+            layers[i].forward(layers[i - 1].output);
+        }
+    }
+};
